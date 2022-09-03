8f1141c502869e121324b0122a50003b4cc460d0
“charbelsan” <dacahomos95@gmail.com>
2022-09-03 18:44:19 +0200

    dataset

---
+++ data_processing.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  3 16:22:26 2022
+
+@author: Charbel
+"""
+
+"""
+The data set containing different leaves of healthy and unhealthy crops
+
+
+This code is used to parse the dataset and extract corn leaf images from the dataset
+"""
+import os
+print(os.listdir("../input/"))
